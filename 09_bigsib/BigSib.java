46533f9fcfaf21444f11720bd87286474d0f4d7e
Hugo Jenkins <hugojenkins@hugos-mbp.lan>
2021-10-02 17:11:50 -0400

    Homework09

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,19 @@
+//KHB- Hugo Jenkins, Kaitlin Ho, Boary, Apple
+//APCS
+//HW09--What Are BigSibs Good For?
+//2021-10-02
+public class BigSib {
+  public static void  greet(String n) {
+    System.out.println("Howdy, "+ n);
+  }
+}
+/* Discoveries:
+You must compile both files before you can run the code
+<class>.<method> runs that method
+This is a situation that you don't need a main method
+QCC:
+How would this work accessing a private class?
+Does a method need a potential argument type?
+e.g. "public static void main( String[] args )"
+-we have no intention of using an argument
+*/
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,11 @@
+//KHB- Hugo Jenkins, Kaitlin Ho, Boary, Apple
+//APCS
+//HW09--What Are BigSibs Good For?
+//2021-10-02
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Flim");
+        BigSib.greet("Flam");
+        BigSib.greet("Kazaam");
+    }
+}

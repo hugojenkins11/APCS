5267a1d7510b503783b35cb246dec4fec7a692ed
Hugo Jenkins <hugojenkins@hugos-mbp.lan>
2021-10-04 18:40:29 -0400

    HW10

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,11 @@
+//KHB- Hugo Jenkins, Kaitlin Ho, Boary, Apple
+//APCS
+//HW09-- Refactor Big Sib One
+//2021-10-03
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}

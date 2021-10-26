33909f6c18f2ca753f5b27bbb8ab7296b5e0b4a0
Hugo Jenkins <hugojenkins@hugos-mbp.lan>
2021-10-25 20:49:13 -0400

    HW24

---
+++ 24_while/Driver.java
@@ -0,0 +1,14 @@
+public Class Driver(){
+  private int heads,tails;
+
+public static void main (String [] args){
+
+Coin otto=new Coin();
+Coin notto = new Coin();
+while otto.headsCtr<heads{
+  otto.flip();
+
+}
+
+}
+}

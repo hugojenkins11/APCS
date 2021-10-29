80058b35ee3c51343e4f5eca8d459575e4f7b836
Hugo Jenkins <hugojenkins@hugos-mbp.lan>
2021-10-28 21:05:13 -0400

    HW 27

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -1,18 +1,20 @@
-/**
-   NIWWD: Hugo Jenkins + Boary>, Jeffery Tang + Mathias, Melody Lew + <duck name>
+/*
+   NIWWD: Hugo Jenkins + Boary, Jeffery Tang + Mathias, Melody Lew + Ollie
    APCS
    HW27 -- FOR the Love of Strings
    2021-10-28
-   time spent: 0.8
-**/
+   time spent: 0.8 hrs
+*/
 
-/**
-   DISCO:
-
-   QCC:
-
-**/
-
+/*
+DISCO:
+0. length is equivalent to the number of 16-bit Unicode of the string.
+1. substring can be used to return part of a string, or a substring of a string. The first input is at which index the substring begins,
+the second input is at which index the substring ends.
+QCC:
+0. What happens if the first input of the substring is a negative integer?
+1. Likewise, what happens if the second input of the substring is greater than the length of the string?
+*/
 
 public class Rof {
 

3044f62c9df83cba7d0d4a527cfe1a911cb5f6d7
Hugo Jenkins <hugojenkins@Hugos-MacBook-Pro.local>
2021-11-19 22:10:11 -0500

    Getting rid of .DS_Store

---
+++ YoRPG/Monster.java
@@ -0,0 +1,19 @@
+public class Monster {
+  public int health;
+  private int damage;
+
+	public Monster() {
+    health = (int) (Math.random()*10+15);
+		health = 20;
+  }
+
+  public boolean isAlive() {
+    return health > 0;
+  }
+  public int attack(Protagonist pro){
+    damage = (int) (Math.random()*10 + 5);
+  	pro.health = pro.health - (int) (damage*pro.vuln);
+	  return damage;
+  }
+
+}

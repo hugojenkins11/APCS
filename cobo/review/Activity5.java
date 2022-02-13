49fa9e73dd29fdd89aba8d6c281aece4a61a01e4
hugojenkins11 <hjenkins30@stuy.edu>
2022-02-13 13:41:37 -0500

    lab06

--- cobo/review/Activity5.java
+++ cobo/review/Activity5.java
@@ -13,28 +13,70 @@ import java.io.*;
 
 
 public class Activity5 {
-  public static void main(String[] args) {
-    String text = textToString("GreatGatsby.txt");
-    
-  }
+    private static final String SPACE = " ";
 
-  public static String textToString(String fileName) {
-      String temp = "";
-      try {
-          Scanner input = new Scanner(new File(fileName));
+    public static String removePunctuation(String word) {
+        while (word.length() > 0 && !Character.isAlphabetic(word.charAt(0))) {
+            word = word.substring(1);
+        }
+        while (word.length() > 0 && !Character.isAlphabetic(word.charAt(word.length() - 1))) {
+            word = word.substring(0, word.length() - 1);
+        }
 
-          // add 'words' in the file to the string, separated by a single space
-          while (input.hasNext()) {
-              temp = temp + input.next() + " ";
-          }
-          input.close();
+        return word;
+    }
 
-      } catch (Exception e) {
-          System.out.println("Unable to locate " + fileName);
-      }
-      // make sure to remove any additional space that may have been added at the end
-      // of the string.
-      return temp.trim();
-  }
+
+    public static String getMostCommonWord(String fileName) {
+        HashMap<String, Integer> wordCounts = new HashMap<String, Integer>();
+
+        String text = textToString(fileName);
+
+        String mostCommonWord = text.split(SPACE)[0];
+        // this line is needed so that line 49 doesn't return a java.lang.NullPointerException
+        wordCounts.put(text.split(SPACE)[0], 0);
+
+        for (String word : text.split(SPACE)) {
+            word = removePunctuation(word);
+            word = word.toLowerCase();
+
+            if (wordCounts.get(word) == null) {
+                wordCounts.put(word, 1);
+            } else {
+                wordCounts.put(word, wordCounts.get(word) + 1);
+            }
+
+            // check if this word is now the most common word
+            if (wordCounts.get(word) > wordCounts.get(mostCommonWord)) {
+                mostCommonWord = word;
+            }
+        }
+
+        return mostCommonWord;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMostCommonWord("GreatGatsby.txt"));
+    }
+
+    public static String textToString(String fileName) {
+        String temp = "";
+        try {
+            Scanner input = new Scanner(new File(fileName));
+
+            // add 'words' in the file to the string, separated by a single space
+            while (input.hasNext()) {
+                temp = temp + input.next() + " ";
+            }
+            input.close();
+
+        } catch (Exception e) {
+            System.out.println("Unable to locate " + fileName);
+        }
+        // make sure to remove any additional space that may have been added at the end
+        // of the string.
+        return temp.trim();
+    }
 
 }

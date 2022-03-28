fd80a8c00bb472cda86c1a2a9cf8229d12d2aee5
Hugo Jenkins <hjenkins30@stuy.edu>
2022-03-28 12:32:07 -0400

    heu

--- 83_stack/Latkes.java
+++ 83_stack/Latkes.java
@@ -22,67 +22,85 @@ public class Latkes
   //constructor
   public Latkes( int initCapacity )
   {
+    _stack = new String[initCapacity];
+    _stackSize = 0;
     /* your
        SIMPLE
        SMART
        magicks
        here
     */
-  }// O(?)
+  }// O(1)
 
 
   //means of insertion
   public void push( String s )
   {
+    if (!isFull()) {
+    _stack[_stackSize] = s;
+    _stackSize++;
+    }
     /* your
        SIMPLE
        SMART
        magicks
        here
     */
-  }// O(?)
+  }// O(1)
 
 
   //means of removal
   public String pop( )
   {
-    /* your
-       SIMPLE
-       SMART
-       magicks
-       here
-    */
-  }// O(?)
+    if (!isEmpty()) {
+    String ret = _stack[0];
+    String[] temp = new String[_stack.length];
+    for (int i = 1; i < _stack.length; i++) {
+      temp[i] = _stack[i];
+    }
+    _stack = temp;
+    _stackSize--;
+    return ret;
+    }
+  return "empty";
+  /* your
+     SIMPLE
+     SMART
+     magicks
+     here
+  */
+  }// O(n)
 
 
   //chk for emptiness
   public boolean isEmpty()
   {
+    return (_stackSize == 0);
     /* your
        SIMPLE
        SMART
        magicks
        here
     */
-  }// O(?)
+  }// O(1)
 
 
   //chk for fullness
   public boolean isFull()
   {
+    return (_stackSize == _stack.length);
     /* your
        SIMPLE
        SMART
        magicks
        here
     */
-  }// O(?)
+  }// O(1)
 
 
   //main method for testing
   public static void main( String[] args )
   {
-    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
 
     Latkes tastyStack = new Latkes(10);
 
@@ -126,6 +144,8 @@ public class Latkes
 
     //stack empty by now; SOP(null)
     System.out.println( tastyStack.pop() );
+    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+
       ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
 
   }//end main()

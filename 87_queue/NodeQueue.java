f5217a62f88f959f95050c0f048586f1ab55aef2
Hugo Jenkins <hjenkins30@stuy.edu>
2022-04-01 12:31:50 -0400

    ok

---
+++ 87_queue/NodeQueue.java
@@ -0,0 +1,13 @@
+public class NodeQueue<Quasar> {
+
+  public QUASAR dequeue() {
+
+  }
+  public void enqueue (QUASAR x) {
+
+  }
+  public boo
+  public static void main(String[] args) {
+
+  }
+}

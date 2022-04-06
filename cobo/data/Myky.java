65cf4183726f48837b75486d2c1169e6add97ef5
Hugo Jenkins <hjenkins30@stuy.edu>
2022-04-06 12:03:39 -0400

    porg

---
+++ cobo/data/Myky.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+// Which country is the most generous and in what year?
+public class Myky {
+
+private String _country;
+private int _year;
+private double _lifeLadder;
+private double _logGDP;
+private double _socialSupport;
+private double _lifeExpectancy;
+private double _freedom;
+private double _generosity;
+private double _perceptions;
+private double _positiveAff;
+private double _negativeAff;
+
+  public Myky(String c, int y, double ll, double lGDP, double ss, double le, double f, doubl g, double p, double pa, double na) {
+    _country = c;
+    _year = y;
+    _lifeLadder = ll;
+    _logGDP = lGDP;
+    _socialSupport = ss;
+    _lifeExpectancy = le;
+    _freedom = f;
+    _generosity = g;
+    _perceptions = p;
+    _positiveAff = pa;
+    _negativeAff = na;
+  }
+  public static void main(String[] args) {
+    ArrayList<String> dat = new ArrayList<String>(3);
+    Scannner sc = new Scanner(System.in);
+    dat.set()
+    while ( sc.hasNext() ) {
+      Myky
+    }
+  }
+}

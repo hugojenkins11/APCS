41f1614d1c8b4c0fa48dec560c3e0bfe88a39a84
hugojenkins11 <hjenkins30@stuy.edu>
2022-04-13 06:59:23 -0400

    oks

---
+++ 90_deque/Deque.java
@@ -0,0 +1,17 @@
+public interface Deque<FORTNITE> {
+  public void addFirst(FORTNITE x);
+
+  public void addLast(FORTNITE x);
+
+  public FORTNITE removeFirst();
+
+  public FORTNITE removeLast();
+
+  public FORTNITE peekFirst();
+
+  public FORTNITE peekLast();
+
+  public FORTNITE pollFirst();
+
+  public FORTNITE pollLast();
+}
